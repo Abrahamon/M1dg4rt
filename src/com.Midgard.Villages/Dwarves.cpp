8a6a24f5d8db66f87fd944233119ddbf8a88c643
abrahamon <Abrahamon@users.noreply.github.com>
2015-04-23 22:55:52 -0600

    nuevas classes fitnes  y Dwarves

---
+++ src/com.Midgard.Villages/Dwarves.cpp
@@ -0,0 +1,18 @@
+/*
+ * Dwarves.cpp
+ *
+ *  Created on: Apr 23, 2015
+ *      Author: abrahamon
+ */
+
+#include "../com.Midgard.Villages/Dwarves.h"
+
+Dwarves::Dwarves() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Dwarves::~Dwarves() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/com.Midgard.Villages/Dwarves.h
@@ -0,0 +1,17 @@
+/*
+ * Dwarves.h
+ *
+ *  Created on: Apr 23, 2015
+ *      Author: abrahamon
+ */
+
+#ifndef COM_MIDGARD_VILLAGES_DWARVES_H_
+#define COM_MIDGARD_VILLAGES_DWARVES_H_
+
+class Dwarves {
+public:
+	Dwarves();
+	virtual ~Dwarves();
+};
+
+#endif /* COM_MIDGARD_VILLAGES_DWARVES_H_ */
