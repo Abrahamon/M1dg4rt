b185a38bed5802a311c39ac1ce6d1c98a5cc1427
fasm22 <fasm2296@gmail.com>
2015-04-25 15:59:43 -0600

    Se añade la clase Entity

---
+++ src/com.Midgard.Entities/Entity.cpp
@@ -0,0 +1,52 @@
+/*
+ * Entity.cpp
+ *
+ *  Created on: Apr 25, 2015
+ *      Author: fabian
+ */
+
+#include "Entity.h"
+
+Entity::Entity(bool pGender, Entity* pFather, Entity* pMother,Entity* pBrother,
+		       LinkedList<short>* pGenome, int pLife) {
+	_Gender = pGender;
+	_Father = pFather;
+	_Mother = pMother;
+	_Brother = pBrother;
+	_Genome = pGenome;
+	_Age = 0;
+	_Experience = 0;
+	_Life = pLife;
+	_Superstition = pMother->getSuperstition();
+
+}
+
+Entity::~Entity() {}
+
+//Entity's Getters
+int Entity::getAge(){ return _Life; }
+int Entity::getExp(){ return _Experience; }
+int Entity::getLife(){ return _Life; }
+bool Entity::getGender(){ return _Gender; }
+int Entity::getSuperstition(){ return _Superstition; }
+LinkedList<short>* Entity::getGenome(){ return _Genome;}
+
+//Entity's Setters
+
+void Entity::Birthday(){ _Age++; }
+void Entity::gainExperience(int pExp){ _Experience+=pExp; }
+void Entity::gainSuperstition(int pSupr){ _Superstition+=pSupr; }
+void Entity::gainLife(int pLife){ _Life+=pLife; }
+
+//Fight Methods
+void Entity::Attack(Entity* pDefender){
+	//Aplicar random aquí
+	int pDamage = 0;
+	pDefender->Defend(pDamage);
+}
+void Entity::Defend(int pDamage){
+	//Aplicar random aquí
+	_Life-=pDamage;
+}
+void Entity::Blot(){}
+void Entity::invokeGod(){}
