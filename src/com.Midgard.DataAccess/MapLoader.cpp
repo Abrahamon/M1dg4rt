38420a6629dc66dc49f28fb09b524caf8c547c07
fasm22 <fasm2296@gmail.com>
2015-04-29 17:16:12 -0600

    Se añade la clase que permite la lectura de los mapas. Se añade el editor de mapas

---
+++ src/com.Midgard.DataAccess/MapLoader.cpp
@@ -0,0 +1,18 @@
+/*
+ * MapLoader.cpp
+ *
+ *  Created on: Apr 29, 2015
+ *      Author: fabian
+ */
+
+#include "MapLoader.h"
+
+MapLoader::MapLoader() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MapLoader::~MapLoader() {
+	// TODO Auto-generated destructor stub
+}
+
