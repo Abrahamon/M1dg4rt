2e5cf3b3bda4064ed087af077d25462b0a19d31d
fasm22 <fasm2296@gmail.com>
2015-04-30 12:09:21 -0600

    Se añade la clase PyArray que mediante lista, intenta simular el comportamiento de un Array. Además se modificaron las clases MapLoader y World, pero no tienen mayor funcionamiento.

--- src/com.Midgard.DataAccess/MapLoader.h
+++ src/com.Midgard.DataAccess/MapLoader.h
@@ -7,11 +7,20 @@
 
 #ifndef SRC_COM_MIDGARD_DATAACCESS_MAPLOADER_H_
 #define SRC_COM_MIDGARD_DATAACCESS_MAPLOADER_H_
+#include "../com.Midgard.Logic/World.h"
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
 
 class MapLoader {
+private:
+	std::ifstream CurrentMap;
 public:
 	MapLoader();
 	virtual ~MapLoader();
+	void loadMap();
+	void readMapMatrix();
 };
 
 #endif /* SRC_COM_MIDGARD_DATAACCESS_MAPLOADER_H_ */
