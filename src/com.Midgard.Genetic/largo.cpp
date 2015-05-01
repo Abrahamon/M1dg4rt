648be0dc59c14def056813fd62f18936de1cec80
mauricio <maumonteroj@gmail.com>
2015-05-01 04:41:20 -0600

    se arreglo el error de la funcion de elevar

--- src/com.Midgard.Genetic/largo.cpp
+++ src/com.Midgard.Genetic/largo.cpp
@@ -4,12 +4,13 @@
  *  Created on: May 01, 2015
  *      Author: Mauricio
  */
-#include "largo.cpp"
+#include "largo.h"
+#include <math.h>
 
 int largo::tamano(uint16_t numero){
 	int n=0;
 	for(int i=0;i<=16;i++){
-		if (numero>2**n){
+		if (numero>pow(2,n)){
 			n=n+1;
 			break;
 		}
